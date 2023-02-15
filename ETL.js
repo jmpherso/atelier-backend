f98896f0a532075dedba0a85c096378c1170984d
Jerry MacPherson <jmpherso@gmail.com>
2023-02-14 18:41:46 -0600

    Basic ETL code to convert CSV files into Postgres tables

---
+++ ETL.js
@@ -0,0 +1,31 @@
+
+const { Client } = require('pg');
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'reviewsandratings',
+    password: 'pokemon10',
+    port: 5432,
+});
+
+client.connect();
+
+client.query('COPY reviews FROM \'/home/jmpherso/course/RatingsAPI/reviews.csv\' DELIMITER \',\' CSV HEADER', (err, res) => {
+    console.log(err, res);
+    client.end();
+})
+
+client.query('COPY characteristics FROM \'/home/jmpherso/course/RatingsAPI/characteristics.csv\' DELIMITER \',\' CSV HEADER', (err, res) => {
+    console.log(err, res);
+    client.end();
+})
+
+client.query('COPY characteristic_reviews FROM \'/home/jmpherso/course/RatingsAPI/characteristic_reviews.csv\' DELIMITER \',\' CSV HEADER', (err, res) => {
+    console.log(err, res);
+    client.end();
+})
+
+client.query('COPY reviews_photos FROM \'/home/jmpherso/course/RatingsAPI/reviews_photos.csv\' DELIMITER \',\' CSV HEADER', (err, res) => {
+    console.log(err, res);
+    client.end();
+})
