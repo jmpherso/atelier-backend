fc9f4e10371d498f9d6d1c8ea8eae8314e85ef5b
Jerry MacPherson <jmpherso@gmail.com>
2023-02-21 15:25:31 -0600

    Removed logger middleware to test for causing slowdowns.

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
-app.use(logger);
+//app.use(logger);
 app.use(express.json());
 // app.use(express.urlencoded());
 // app.use(express.static(path.join(__dirname, '../public')));
