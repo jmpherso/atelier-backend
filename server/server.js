a94b14477e93d9976e3d44849cac81d2870aee75
Jerry MacPherson <jmpherso@gmail.com>
2023-02-21 14:16:07 -0600

    bugfix

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -9,7 +10,7 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
 app.use(logger);
 app.use(express.json());
